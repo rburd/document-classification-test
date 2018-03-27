17ef5cd9bd082c8da46a103b7f623ef4549af6e7
rburd <rburd@seas.upenn.edu>
2018-03-27 01:44:26 +0100

    Integrate form into index.html

--- classification/views.py
+++ classification/views.py
@@ -1,6 +1,7 @@
 # -*- coding: utf-8 -*-
 from __future__ import unicode_literals
 from django.http import HttpResponse
+from django.http import HttpResponseBadRequest
 from django.http import HttpResponseRedirect
 from django.template import loader
 from sklearn.externals import joblib
@@ -16,6 +17,8 @@ def index(request):
         # check whether it's valid:
         if form.is_valid():
             #process submitted text
+            words = form.cleaned_data['words']
+            print(words)
 
             #Todo: encode words into redirected url
             return HttpResponseRedirect('/classification')
@@ -26,9 +29,16 @@ def index(request):
 
 def classify(request):
     #load classifier
-    classifier = joblib.load("model.p", "wb+")
+    classifier = joblib.load("model.p")
     template = loader.get_template('classication/classify.html')
 
     #get words value and predict
+    query = request.META['QUERY_STRING']
+    pair = str(query).split('=')
+
+    if ('words' not in pair) :
+        return HttpResponseBadRequest("No words parameter found in url")
+    text = pair[1]
+    prediction = classifier.predict(text)
     context = {'here': 'there', }
     return HttpResponse(template.render(context, request))
